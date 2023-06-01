bdc4a5a25192c480fddd1b529b57076f6d0cfccc
sateeva <ateeva.symbat@gmail.com>
2023-06-02 00:31:19 +0200

    name-surname added

--- src/test/java/LoginTests/Test1.java
+++ src/test/java/LoginTests/Test1.java
@@ -3,5 +3,8 @@ package LoginTests;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Project started");
+        String name = " Jack";
+        String surname = "Bauer";
+
     }
 }
