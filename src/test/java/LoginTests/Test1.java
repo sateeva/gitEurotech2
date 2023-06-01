94656f48d90baf78da89c8fbdc2441486426261a
sateeva <ateeva.symbat@gmail.com>
2023-06-02 01:50:17 +0200

    new implementation made

--- src/test/java/LoginTests/Test1.java
+++ src/test/java/LoginTests/Test1.java
@@ -3,14 +3,19 @@ package LoginTests;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Project started");
-        String name = " Jack";
-        String surname = "Bauer";
+        String name = " Ali";
+        String surname = "Öztürk";
 
         //ADDITIONAL INFO
 
-        String number = "6474647";
-        String street = "Hemingway";
+
+        String street = "Bahcelievler";
         String state = "California";
+
+        //STUDENT INFO
+       String nameStudent = "John" ;
+       int studentNo = 224;
+
 
 
     }
