4f84c0f778f80578ee66c44216936dcfa39edba6
sateeva <ateeva.symbat@gmail.com>
2023-06-02 01:56:13 +0200

    some changes made in the master

--- src/test/java/LoginTests/Test1.java
+++ src/test/java/LoginTests/Test1.java
@@ -3,14 +3,12 @@ package LoginTests;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Project started");
-        String name = " Jack";
-        String surname = "Bauer";
 
         //ADDITIONAL INFO
 
-        String number = "6474647";
-        String street = "Hemingway";
-        String state = "California";
+        String number = "00000000";
+        String street = "xxxxxxxx";
+        String state = "xxxxxxxx";
 
 
     }
