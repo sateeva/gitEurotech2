a11c21ca7794eba2f0b1ba8b046205f5470fa0c1
sateeva <ateeva.symbat@gmail.com>
2023-06-02 09:39:52 +0200

    Symbat1 added

--- src/test/java/LoginTests/Symbat1.java
+++ src/test/java/LoginTests/Symbat1.java
@@ -4,5 +4,11 @@ public class Symbat1 {
     public static void main(String[] args) {
         String name = " Baris Manco";
         String song = "Gulpembe";
+
+        //***********
+
+        String coutry = "Germany";
+        String city= "Köln";
+
     }
 }
